4426525e9037cd5004f2d8c059c36388cb214537
Alex Benoit <alexander.benoit@outlook.com>
2016-08-16 14:46:12 +0100

    done task amanger

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,12 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task
+    @task = Task.new
   end
 
   def create
     @task = Task.create(task_params)
+    redirect_to task_params
   end
 
   def edit
@@ -22,15 +23,17 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
-
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
   def task_params
     params.require(:task).permit(:title, :done)
   end
-
 end
